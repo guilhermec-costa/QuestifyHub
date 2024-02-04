3e7fdb6727362f80eae77e9a5119eb202dbf6950
Guilherme Costa <echina725@gmail.com>
2024-02-04 02:38:56 -0300

    chore: server is basically running

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,13 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const app: Express = express();
+const PORT: any|number = process.env.PORT || 3333;
+
+app.use(express.json());
+
+app.listen(PORT, "127.0.0.1", () => {
+    console.log(`QuestifyHub running at port ${PORT}`);
+});
