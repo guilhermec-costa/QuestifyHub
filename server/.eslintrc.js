9d835b1e159f9cc18574ea5941ec4e5c50062013
Guilherme Costa <echina725@gmail.com>
2024-02-04 15:20:11 -0300

    chore: add eslint config

---
+++ server/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    "parser": "@typescript-eslint/parser",
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "plugins": ["@typescript-eslint"],
+    "extends": [
+        "plugin:@typescript-eslint/recommended",
+        "standard-with-typescript",
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        "semi": "always",
+        "quotes": [
+            "error",
+            "single"
+        ]
+    }
+}
