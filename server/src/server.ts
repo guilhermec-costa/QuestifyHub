1bf5238a623c32e76257e268a60f26e489c9fee6
Guilherme China <echina725@gmail.com>
2024-02-14 01:34:20 -0300

    remove dockerfile

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,10 +5,9 @@ import mongoose from "mongoose";
 import indexRouter from "./routes/index";
 
 dotenv.config();
-mongoose.connect(process.env.MONGODB_URI as string, {
-    dbName: process.env.DB_NAME,
-    user: process.env.DB_USER
-})
+mongoose.connect("mongodb://127.0.0.1:27017/Questifyhub")
+    .then(() => console.log("connected to database"))
+    .catch(e => console.log("Error: ", e.message));
     
 const app: Express = express();
 const PORT: any|number = process.env.PORT || 3333;
@@ -16,6 +15,11 @@ const PORT: any|number = process.env.PORT || 3333;
 app.use(express.json());
 app.use(cors());
 app.use(indexRouter);
+
+app.get("/", (req, res) => {
+    console.log("AQUI NO GET");
+    return res.send({msg: "foi"});
+})
 
 app.listen(PORT, () => {
     console.log(`QuestifyHub running at port ${PORT}`);
