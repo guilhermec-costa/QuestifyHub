31b663b9ff9da46d57625acf3170b6ffbc7c40f1
Guilherme China <echina725@gmail.com>
2024-02-17 14:44:05 -0300

    add login screen prototype

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,10 +1,29 @@
 import { Component, createSignal } from "solid-js";
 
 const Login: Component = () => {
+    let loginForm:HTMLFormElement;
+    const handleFormSubmition = (e:Event) => {
+        e.preventDefault();
+        console.log(e.target);
+    }
     return (
-        <div>
-            Olá
-            <input type="text" />
+        <div class="bg-[#FFC107] w-full h-screen flex justify-center items-center">
+            <div class="w-3/5 flex h-3/5">
+                <div class="w-1/2 bg-[#F5F5F5] rounded-l-lg">
+                    <h3>Get started on Questify</h3>
+                    <form ref={loginForm}
+                        class="h-1/2 w-1/2 p-3 border-black mx-auto my-[25%]"
+                        onSubmit={handleFormSubmition}>
+                        <div class="flex flex-col gap-2">
+                            <input  type="email"  placeholder="Email"/>
+                            <input type="password" placeholder="Password" />
+                            <input type="submit" value="Login" class="hover:cursor-pointer"/>
+                        </div>
+                    </form>
+
+                </div>
+                <div class="bg-[#333333] w-1/2 rounded-r-lg">Aqui</div>
+            </div>
         </div>
     )
 }
