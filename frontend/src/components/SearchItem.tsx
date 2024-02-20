68f775e56458a3c455fa6da93fd6fced27a344f2
Guilherme China <echina725@gmail.com>
2024-02-20 18:52:19 -0300

    feat: adds dynamic data

--- frontend/src/components/SearchItem.tsx
+++ frontend/src/components/SearchItem.tsx
@@ -2,10 +2,13 @@ import { Component } from "solid-js";
 import { TSearchProps } from "../types/SearchProps";
 
 const SearchItem: Component<TSearchProps> = ({title, displayLink, snippet, position}) => {
-    console.log(displayLink);
     return (
-        <div class="py-3">
-            <h3 class="text-[#B4CDED] text-xl font-bold"><a href={displayLink} target="_blank">{title}</a></h3>
+        <div class="p-5 border border-[#344966]">
+            <h3 class="text-[#B4CDED] text-xl font-bold hover:underline decoration-1 decoration-[#B4CDED]">
+                <a href={displayLink} target="_blank">
+                    {title}
+                </a>
+            </h3>
             <p class="text-[#F0F4EF] text-lg ml-8 py-2">{snippet}</p>
         </div>
     )
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,40 +1,46 @@
-import { Component, For} from "solid-js";
+import { Component, For, Show, createEffect, onMount} from "solid-js";
 import { checkAuthentication } from "../utils/auth";
 import { useNavigate } from "@solidjs/router";
 import { Telescope } from "lucide-solid";
 import HomeProfileMenu from "../components/HomeProfileMenu";
 import SearchItem from "../components/SearchItem";
 import data from "../data.json";
+import { createStore } from "solid-js/store";
 
 const Home: Component = () => {
     const navigator = useNavigate();
     const username = localStorage.getItem("username");
     checkAuthentication(navigator);
-    const { items:searchItems } = data;
-    console.log(searchItems);
 
+    const [searchItems, setSearchItems] = createStore<Object[]>([]);
+
+    onMount(() => {
+        setSearchItems(data.items);
+    })
 
     return (
         <div class="bg-[#0D1821] relative">
             <HomeProfileMenu />
-            <div class="w-4/5 mx-auto flex flex-col justify-center items-center">
+            <div class="min-w-4/5 mx-auto flex flex-col justify-center items-center">
                 <form method="post" class="w-[40%] mt-[200px] relative">
                     <input type="text"
                        placeholder="Type anything"
-                       class="pl-6 py-3 w-full rounded-md text-xl outline-none hover:outline-none"/>
+                       class="pl-6 py-3 w-full rounded-md text-xl outline-none hover:outline-none border-3 border-[#cccbc8]"/>
                     <Telescope width={48} height={30} class="absolute right-3 bottom-[7px]" color="#344966"/>
                 </form>
                 <div class="py-12">
-                    <For each={searchItems}>
-                        {(search, i) => (
-                            <SearchItem
-                                displayLink={search.displayLink}
-                                snippet={search.snippet}
-                                title={search.title}
-                                position={i()}
-                            />
-                        )}
-                    </For>
+                    <Show when={searchItems}>
+                        <For each={searchItems}>
+                            {(search:any, i) => (
+                                <SearchItem
+                                    displayLink={search.link}
+                                    snippet={search.snippet}
+                                    title={search.title}
+                                    position={i()}
+                                />
+                            )}
+                        </For>
+                    </Show>
                 </div>
             </div>
         </div>
