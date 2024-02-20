daf7ad349dea93ab464aaddfd97f7e42f9695a5c
Guilherme China <echina725@gmail.com>
2024-02-20 19:16:30 -0300

    feat: adds search on different types of events

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Component, For, Show, createEffect, onMount} from "solid-js";
+import { Component, For, Show, createEffect, onCleanup, onMount} from "solid-js";
 import { checkAuthentication } from "../utils/auth";
 import { useNavigate } from "@solidjs/router";
 import { Telescope } from "lucide-solid";
@@ -10,23 +10,32 @@ import { createStore } from "solid-js/store";
 const Home: Component = () => {
     const navigator = useNavigate();
     const username = localStorage.getItem("username");
-    checkAuthentication(navigator);
-
     const [searchItems, setSearchItems] = createStore<Object[]>([]);
 
-    onMount(() => {
-        setSearchItems(data.items);
-    })
+    checkAuthentication(navigator);
+    onMount(() => document.addEventListener("keydown", handleSearch));
+    onCleanup(() => document.removeEventListener("keydown", handleSearch));
+
+    const handleSearch = (event:Event) => {
+        const {items} = data;
+        switch(event.type) {
+            case "keydown": {
+                const { key } = (event as KeyboardEvent);
+                if(key==="Enter") setSearchItems(items);
+            };
+
+            default: setSearchItems(items);
+        };
+    };
 
     return (
-        <div class="bg-[#0D1821] relative">
+        <div class="min-h-screen bg-[#0D1821] relative">
             <HomeProfileMenu />
             <div class="min-w-4/5 mx-auto flex flex-col justify-center items-center">
                 <form method="post" class="w-[40%] mt-[200px] relative">
                     <input type="text"
-                       placeholder="Type anything"
-                       class="pl-6 py-3 w-full rounded-md text-xl outline-none hover:outline-none border-3 border-[#cccbc8]"/>
-                    <Telescope width={48} height={30} class="absolute right-3 bottom-[7px]" color="#344966"/>
+                       placeholder="Type anything" class="pl-6 py-3 w-full rounded-md text-xl outline-none hover:outline-none border-3 border-[#cccbc8]"/>
+                    <Telescope width={48} height={30} class="absolute right-3 bottom-[9px] hover:cursor-pointer" color="#344966" onClick={handleSearch}/>
                 </form>
                 <div class="py-12">
                     <Show when={searchItems}>
