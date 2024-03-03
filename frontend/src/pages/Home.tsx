72dbcc6e0b1095915ba2e4e2ffe99821bd881c73
Guilherme China <echina725@gmail.com>
2024-03-03 15:02:15 -0300

    chore: change collapsible style

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -93,30 +93,17 @@ const Home: Component = () => {
                         <Telescope width={48} height={30} class="hover:cursor-pointer absolute right-2 z-20" color="#344966" onClick={handleSearch}/>
                     </form>
                     <div class="w-full relative mx-auto">
-                        {/* <div class="mt-[20px] w-[80%] bg-slate-600 rounded-sm inline-block"> */}
-                        {/*     <label> */}
-                        {/*         <div class="flex justify-between items-center px-2 bg-slate-700 rounded-sm hover:bg-slate-500"> */}
-                        {/*             <h4 class="p-2 text-[#F0F4EF] flex gap-x-3 items-center cursor-pointer w-full" */}
-                        {/*             onClick={(e:Event) =>{ */}
-                        {/*                 e.stopPropagation(); */}
-                        {/*                 setIsCustomSearchExpanded(prev => !prev)} */}
-                        {/*             }> */}
-                        {/*                 Customize your search<SlidersHorizontal width={18} /></h4> */}
-                        {/*             {!isCustomSearchExpanded() ? ( */}
-                        {/*                 <ChevronsDown color="#ffffff" class="cursor-pointer"/> */}
-                        {/*             ) : ( */}
-                        {/*                 <ChevronsUp color="#ffffff" class="cursor-pointer"/> */}
-                        {/*             )} */}
-                        {/*         </div> */}
-                        {/*         <input type="checkbox"/> */}
-                        {/*             <div class="collapsed"> */}
-                        {/*             <p class="p-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus vero earum quae numquam vel! Fugiat, molestias quibusdam neque repellendus debitis dolorum. Veniam consectetur tenetur omnis ex cupiditate, ratione libero ullam.</p></div> */}
-                        {/*    </label> */}
-                        {/* </div> */}
                         <div class="w-4/5 mt-[20px] rounded-sm inline-block">
-                           <button class="p-2 cursor-pointer bg-slate-600 w-full rounded-t-sm" onClick={() => setIsCustomSearchExpanded(prev => !prev)}>Collapsible</button>
-                               <div class={`${isCustomSearchExpanded() ? "h-[200px] overflow-auto" : "h-[0px] overflow-hidden"} content bg-slate-500 w-full`}>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae esse alias maiores repellat officiis praesentium omnis, quos aliquam earum porro eveniet velit ullam aut mollitia accusamus assumenda cum veritatis delectus?</p>
+                           <button class="p-2 cursor-pointer bg-slate-800 w-full rounded-t-sm flex justify-between hover:bg-slate-700 border-b-2 border-sky-200" onClick={() => setIsCustomSearchExpanded(prev => !prev)}>
+                               <p class="flex justify-start gap-x-3 items center text-[#B4CDED] ml-3">Customize your search<SlidersHorizontal width={18} color="#B4CDED"/></p>
+                                {!isCustomSearchExpanded() ? (
+                                    <ChevronsDown color="#ffffff" class="cursor-pointer"/>
+                                ) : (
+                                    <ChevronsUp color="#ffffff" class="cursor-pointer"/>
+                                )}
+                           </button>
+                               <div class={`${isCustomSearchExpanded() ? "h-[200px] overflow-auto" : "h-0 overflow-hidden"} content bg-slate-800 w-full`}>
+                                    <p class="text-[#F0F4EF] p-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae esse alias maiores repellat officiis praesentium omnis, quos aliquam earum porro eveniet velit ullam aut mollitia accusamus assumenda cum veritatis delectus?</p>
                                </div>
                         </div>
                         <button class="text-[#0D1821] bg-[#BFCC94] p-2 rounded-md w-[18%] absolute top-[20px] right-0 flex justify-center items-center gap-x-2 hover:bg-[#BFCC99]" onClick={clearCachedContent}>
