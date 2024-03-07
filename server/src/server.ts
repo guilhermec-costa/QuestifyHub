eb00e85edae306c3d4e548e66ed36a1f55bcfc83
Guilherme China <echina725@gmail.com>
2024-03-07 18:53:00 -0300

    feat: add openai configuration

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,12 +4,22 @@ import dotenv from "dotenv";
 import indexRouter from "./routes/index";
 import connectToMongo from "database/db";
 import cookieParser from "cookie-parser";
+import OpenAI from "openai";
 
 dotenv.config();
 
 const app: Express = express();
 const PORT: any|number = process.env.PORT || 3333;
 connectToMongo();
+const openai = new OpenAI({
+    apiKey: process.env.OPEN_API_KEY
+});
+
+/* const completion = openai.chat.completions.create({ */
+/*     messages: [{ role: "system", content: "You are a helpful assistant." }], */
+/*     model: "gpt-3.5-turbo", */
+/* }); */
+/* console.log(completion.then(response => response.choices[0])); */
 
 const corsOptions = {
   origin: '*',
